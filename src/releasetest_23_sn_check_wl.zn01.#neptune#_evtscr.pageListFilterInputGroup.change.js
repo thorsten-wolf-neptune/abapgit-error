96668849f4b6305f909d65a6f5fba1e881ba1d50
Thorsten Wolf <thorsten.wolf@neptune-software.com>
2023-11-16 20:08:09 +0100

    minified

--- src/releasetest_23_sn_check_wl.zn01.#neptune#_evtscr.pageListFilterInputGroup.change.js
+++
@@ -1,1 +0,0 @@
-pageListFilterInputSort.fireChange(pageListFilterInputSort);
--- src/releasetest_23_sn_check_wl.zn01.#neptune#_evtscr.pageListFilterInputSort.change.js
+++
@@ -1,7 +0,0 @@
-var sorters = [];
-var asc = (pageListFilterInputOrder.getSelectedKey() === "DESC");
-var binding = TabData.getBinding("items");
-
-if (pageListFilterInputGroup.getSelectedKey()) sorters.push(new sap.ui.model.Sorter(pageListFilterInputGroup.getSelectedKey(), asc, true));
-if (pageListFilterInputSort.getSelectedKey()) sorters.push(new sap.ui.model.Sorter(pageListFilterInputSort.getSelectedKey(), asc, false));
-binding.sort(sorters);
